7ef35ad8ef49f7d79a5d4ce0f5552101c7d06fe4
Petr Veigend <iveigend@fit.vut.cz>
2022-09-27 12:31:24 +0200

    skript upraven tak, aby zahrnul i výuku, která se neodehrává na Božetěchové

--- vyuka_den/prehled.py
+++ vyuka_den/prehled.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from operator import ge
 import os
 import sys
 
@@ -11,9 +12,24 @@ sys.path.append(PROJECT_ROOT)
 
 import utils
 
-def getSchedule(s, room, date):
+def getCourses(s):
+    url = f"https://api.vut.cz/api/fit/predmety/v3?lang=cs&rok=2022&typ_semestru_id=2"
+    data = s.get(url).json()['data']['predmety']
+
+    out = []
+    for c in data:
+        course = {}
+        course['zkratka'] = c['zkratka']
+        for ca in c['aktualni_predmety']:
+            course['aktualni_predmet_id'] = ca['aktualni_predmet_id']
+            if ca['aktualni_pocet_studentu'] > 0:
+                out.append(course)    
+
+    return out
+
+def getScheduleCourse(s, course, date):
     date_is = f"{date.split('.')[2]}-{date.split('.')[1]}-{date.split('.')[0]}T00:00:00"
-    url = f"https://api.vut.cz/api/fit/mistnost/{room}/vyucovani/v3?lang=cs&rok=2022&datum_od={date}&datum_do={date}"
+    url = f"https://api.vut.cz/api/fit/aktualni_predmet/{course}/vyucovani/v3?lang=cs&rok=2022&datum_od={date}&datum_do={date}"
     print(url)
     data = s.get(url).json()['data']
    
@@ -40,20 +56,19 @@ def getSchedule(s, room, date):
                 out.append(tmp)
     
     return out
-        
 
 ID = 110633
 DATE = '17.11.2022' 
 s = utils.createSession(ID)
-rooms = utils.getRooms(s)
+courses = getCourses(s)
 
 current_time = datetime.now().strftime("%m-%d-%Y_%H%M%S")
 with open(f'{DATE}-{current_time}.txt', 'w', encoding='utf-8') as f:
-    for r in rooms: 
-        print(r)
-        schedule = getSchedule(s,r['id'],DATE)
+    for c in courses: 
+        print(c)
+        schedule = getScheduleCourse(s,c['aktualni_predmet_id'],DATE)
         if len(schedule) > 0: 
-            f.write(f"== {r['name']} == \n")
+            f.write(f"== {c['zkratka']} == \n")
         for sch in schedule:
             url = f"https://www.vut.cz/teacher2/cs/predmet-rozvrh-editace/detail/id/{sch['id']}"
-            f.write(f"{sch['predmet']}, {sch['typ']}, {sch['od']} -- {sch['do']}: {url}\n")
+            f.write(f"{sch['typ']}, {sch['od']} -- {sch['do']}: {url}\n")
